2125baf6936f19852f1a558ad4a9ce767f6c7e70
Alec M <alecmattu1234@gmail.com>
2021-12-11 19:50:34 -0500

    Initial commit

---
+++ Parser.class.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * Produced: Sat Dec 11 2021
+ * Author: Alec M.
+ * GitHub: https://amattu.com/links/github
+ * Copyright: (C) 2021 Alec M.
+ * License: License GNU Affero General Public License v3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Class Namespace
+namespace amattu;
+
+/*
+ * Configuration/Ini file parser class
+ */
+class Parser {
+	/**
+   * Holds the parsed configuration data
+   *
+   * @var array
+   */
+	protected $config;
+
+  /**
+   * An array of the sections found in the configuration file
+   *
+   * @var ?bool
+   */
+	protected $sections;
+
+	/**
+	 * Configuration reader constructor
+	 *
+	 * @param string $file The path to the configuration file
+	 * @param ?bool $sections Whether to parse individual sections or not
+	 * @throws TypeError
+   * @author Alec M.
+	 */
+	public function __construct(string $filename, ?bool $sections = false)
+{
+		// Variables
+		$this->sections = $sections;
+		$this->config = $this->read($filename);
+	}
+
+	/**
+	 * Get configuration file property
+	 *
+	 * @param string $key The property to get
+	 * @param string|null $section The section to get the property from
+	 * @param ?mixed $default value to return if the property is not found
+	 * @return $config[$key] || $default || null
+	 * @throws TypeError
+	 * @author Alec M.
+	 */
+	public function get(string $key, ?string $section = null, ?mixed $default = null) : mixed
+  {
+		if ($section && array_key_exists($section, $this->config) && array_key_exists($key, $this->config[$section])) {
+			return $this->config[$section][$key];
+		}
+		if (array_key_exists($key, $this->config)) {
+			return $this->config[$key];
+		}
+
+		// Return
+		return $default;
+	}
+
+	/**
+	 * Read configuration file
+	 * @param string $file location
+	 * @return Array Parsed File
+	 * @throws TypeError
+	 * @author Alec M.
+	 */
+	protected function read(string $file) : array
+  {
+		// Checks
+		if (!file_exists($file)) {
+			return Array();
+		}
+		if (!function_exists("parse_ini_file")) {
+			return Array();
+		}
+
+		// Return
+		return parse_ini_file($file, $this->sections) ?: Array();
+	}
+}
--- README.md
+++ README.md
@@ -1,14 +1,55 @@
 # Introduction
-N/A
+This is a basic configuration/`.ini` file parser built with PHP. It's designed to be error-safe, and provides the opportunity to use a default value if one is not found.
 
 # Usage
-N/A
+## Setup
 
-# Notes
-N/A
+```PHP
+require(__DIR__ . "/Parser.class.php");
+
+$config = new amattu\Parser(__DIR__ . "/conf.ini");
+```
+
+## construct
+The class constructor accepts the file location (path + name), and an optional boolean argument on parsing the individual sections (usually denoted by `[NAME]`).
+
+### PHPDoc
+```PHP
+/**
+ * Configuration reader constructor
+ *
+ * @param string $file The path to the configuration file
+ * @param ?bool $sections Whether to parse individual sections or not
+ * @throws TypeError
+ * @author Alec M.
+ */
+public function __construct(string $filename, ?bool $sections = false)
+```
+
+___
+
+## get
+The `get` function will attempt to pull the value from the section specified (... if specified), and if not found, will return the default parameter. All of the arguments except `$key` are optional.
+
+### Usage
+```PHPDoc
+$usernameOrDefault = $config->get("USERNAME", "GMAIL_SECTION", "defaultUserName");
+```
+
+### PHPDoc
+```PHP
+/**
+ * Get configuration file property
+ *
+ * @param string $key The property to get
+ * @param string|null $section The section to get the property from
+ * @param ?mixed $default value to return if the property is not found
+ * @return $config[$key] || $default || null
+ * @throws TypeError
+ * @author Alec M.
+ */
+public function get(string $key, ?string $section = null, ?mixed $default = null) : mixed
+```
 
 # Requirements & Dependencies
-N/A
-
-# Previews
-![preview image](https://via.placeholder.com/350x240)
+- PHP 7.x +
